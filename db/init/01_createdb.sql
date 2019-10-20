c797aa6c3694b8678f544ac195e6bd520cd9e806
mixmaru <mix.touch777@gmail.com>
2019-10-20 13:24:49 +0900

    first commit

---
+++ db/init/01_createdb.sql
@@ -0,0 +1,8 @@
+create database rails_db;
+grant all privileges on database rails_db to db_user;
+
+create database rails_db_dev;
+grant all privileges on database rails_db_dev to db_user;
+
+create database rails_db_test;
+grant all privileges on database rails_db_test to db_user;
